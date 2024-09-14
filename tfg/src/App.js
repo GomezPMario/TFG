42a5f0faaa171227889e1d746ecf4a467ea98c28
Mario Gómez <alu.124956@usj.es>
2024-09-14 15:00:45 +0200

    My first commit

---
+++ tfg/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import './App.css';
+
+function App() {
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
