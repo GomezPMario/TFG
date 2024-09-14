d083998af15cd143cacde507ee1f502e904b8319
Mario Gómez <alu.124956@usj.es>
2024-09-14 15:11:11 +0200

    Github Pages settings

--- tfg/src/App.js
+++ tfg/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <Routes>
         <Route path="/" element={<Login />} />
       </Routes>
