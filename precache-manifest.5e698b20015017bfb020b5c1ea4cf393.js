89441a96b1d5b468314c051e6fd65e865be40acd
GomezPMario <GomezPMario@users.noreply.github.com>
2024-09-16 19:21:52 +0000

    deploy: a894ba24fe4014ff108bfe4c1d13257719c41a5d

---
+++ precache-manifest.5e698b20015017bfb020b5c1ea4cf393.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2a2ed54cdbf7f499b077bc06e94d361",
+    "url": "/TFG/index.html"
+  },
+  {
+    "revision": "e252212c3e38ef697cea",
+    "url": "/TFG/static/css/main.aa1a95fa.chunk.css"
+  },
+  {
+    "revision": "53b99b6628fd81e514ae",
+    "url": "/TFG/static/js/2.ec535205.chunk.js"
+  },
+  {
+    "revision": "db4976e2c3fc371be79e",
+    "url": "/TFG/static/js/3.1af95c37.chunk.js"
+  },
+  {
+    "revision": "e252212c3e38ef697cea",
+    "url": "/TFG/static/js/main.588573f0.chunk.js"
+  },
+  {
+    "revision": "e31ecda06986ab72e13e",
+    "url": "/TFG/static/js/runtime~main.d63f9eeb.js"
+  },
+  {
+    "revision": "51a7473c58fb77799c80aa5ce72557f8",
+    "url": "/TFG/static/media/LoginWallpaper.51a7473c.jpeg"
+  },
+  {
+    "revision": "735869f8adda19828d329fd068b2a8fe",
+    "url": "/TFG/static/media/LogoCAAB.735869f8.png"
+  }
+]);
