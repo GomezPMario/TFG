08cacf3b67c9a71cc3d5a321fc261510592cce5a
Mario Gómez <alu.124956@usj.es>
2024-09-18 22:22:44 +0200

    reset

--- server/server.js
+++ server/server.js
@@ -50,7 +50,7 @@ app.post('/api/login', (req, res) => {
     const { username, password } = req.body;
     console.log('Received login request:', { username, password });
 
-    const sql = 'SELECT * FROM usuarios WHERE username = ? AND password = ?';
+    const sql = 'SELECT * FROM users WHERE username = ? AND password = ?';
     db.query(sql, [username, password], (err, result) => {
         if (err) {
             console.error('Error querying the database:', err);
