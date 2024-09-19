60ebe0e969a0001566f20726f424317131265ebe
Mario Gómez <alu.124956@usj.es>
2024-09-20 00:42:47 +0200

    .

--- tfg/src/App.js
+++ tfg/src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
-import Consusltas from './components/Consultas';
+import Consultas from './components/Consultas';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/consultas" element={<Consusltas />} />
+        <Route path="/consultas" element={<Consultas />} />
       </Routes>
     </Router>
   );
