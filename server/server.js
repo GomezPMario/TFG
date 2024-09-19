acdbbb962a04a9ab53b3da62dda17c54716a7765
Mario Gómez <alu.124956@usj.es>
2024-09-20 00:41:36 +0200

    :)

--- server/server.js
+++ server/server.js
@@ -8,17 +8,11 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Middleware CORS
 app.use(cors({
-    origin: ['http://localhost:3000', 'https://gomezpmario.github.io'], // Permite localhost y GitHub Pages
-    methods: ['GET', 'POST', 'OPTIONS'], // Asegúrate de permitir los métodos necesarios
-    allowedHeaders: ['Content-Type', 'Authorization'] // Permite headers que puedas necesitar
+    origin: ['http://localhost:3000', 'https://gomezpmario.github.io'], // Añade el origen de GitHub Pages
 }));
-
-// Middleware para procesar solicitudes JSON
 app.use(express.json());
 
-// Conexión a la base de datos MySQL
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
@@ -27,7 +21,6 @@ const connection = mysql.createConnection({
     database: process.env.DB_NAME
 });
 
-// Comprobar la conexión a la base de datos
 connection.connect((err) => {
     if (err) {
         console.error('Error connecting to the database:', err);
@@ -36,22 +29,18 @@ connection.connect((err) => {
     console.log('Connected to the MySQL database');
 });
 
-// Endpoint para obtener usuarios
 app.get('/api/users', (req, res) => {
     const sql = 'SELECT * FROM users';
     connection.query(sql, (err, result) => {
         if (err) {
-            console.error('Error fetching users:', err);
-            return res.status(500).send('Error fetching users');
+            return res.status(500).send(err);
         }
         res.json(result);
     });
 });
 
-// Endpoint para manejar login
 app.post('/api/login', (req, res) => {
     const { username, password } = req.body;
-
     console.log('Received login request:', { username, password });
 
     const sql = 'SELECT * FROM users WHERE username = ? AND password = ?';
@@ -71,7 +60,6 @@ app.post('/api/login', (req, res) => {
     });
 });
 
-// Iniciar servidor
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
