97513c57e3f6ef41a1c671ebfdaa7c34f8f1ab5b
Mario Gómez <alu.124956@usj.es>
2024-09-23 17:23:54 +0200

    :/

--- server/server.js
+++ server/server.js
@@ -25,7 +25,10 @@ const connection = mysql.createConnection({
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: true,
+    }
 });
 
 // Intento de conexión a la base de datos
