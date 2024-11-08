05cb28f68d3aef5b4da42864b817fe7aed11f830
Mario Gómez <alu.124956@usj.es>
2024-11-09 00:58:51 +0100

    tarifas

--- tfg/src/components/Tarifas.jsx
+++ tfg/src/components/Tarifas.jsx
@@ -1,26 +1,24 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import { jsPDF } from 'jspdf';
 import 'jspdf-autotable';
 import axios from 'axios';
 import { baseURL } from './Login';
 import logo from '../components/images/LogoCAAB.png';
+import './styles/Tarifas.css'; // Importa los estilos de Tarifas
+import { FaDownload } from "react-icons/fa6";
 
 const Tarifas = () => {
     const [tarifas, setTarifas] = useState([]);
-    const [showAuxiliar2InPDF, setShowAuxiliar2InPDF] = useState(false); // Estado para controlar la visibilidad de Auxiliar 2 en PDF
+    const [showAuxiliar2InPDF, setShowAuxiliar2InPDF] = useState(false);
     const currentYear = new Date().getFullYear();
     const currentMonth = new Date().getMonth();
     const season = currentMonth >= 8 ? `${currentYear}/${currentYear + 1}` : `${currentYear - 1}/${currentYear}`;
-    const contentRef = useRef();
 
-    // Obtener las tarifas de la API
     useEffect(() => {
         const fetchTarifas = async () => {
             try {
                 const response = await axios.get(`${baseURL}/api/tarifas`);
                 setTarifas(response.data);
-
-                // Verificar si algún elemento tiene valor en Auxiliar_2 para mostrarlo en el PDF
                 const hasAuxiliar2 = response.data.some((tarifa) => tarifa.Auxiliar_2 !== null);
                 setShowAuxiliar2InPDF(hasAuxiliar2);
             } catch (error) {
@@ -31,7 +29,6 @@ const Tarifas = () => {
         fetchTarifas();
     }, []);
 
-    // Convertir imagen a base64 usando un objeto Image
     const loadImageAsBase64 = (src, callback) => {
         const img = new Image();
         img.crossOrigin = 'Anonymous';
@@ -47,26 +44,20 @@ const Tarifas = () => {
         };
     };
 
-    // Función para descargar el PDF
     const downloadPDF = () => {
         const doc = new jsPDF('landscape');
         doc.setFontSize(12);
         const title = `TARIFAS ARBITRAJES - TEMPORADA ${season}`;
         const pageWidth = doc.internal.pageSize.width;
         const titleX = (pageWidth - doc.getTextWidth(title)) / 2;
-
-        // Definir el encabezado según la visibilidad de Auxiliar 2 en PDF
         const head = showAuxiliar2InPDF
             ? [['Categoría', 'Principal', 'Auxiliar 1', 'Auxiliar 2', 'Anotador', 'Crono', '24"', 'Ayud. Anot.', 'Canon FAB', 'Total']]
             : [['Categoría', 'Principal', 'Auxiliar 1', 'Anotador', 'Crono', '24"', 'Ayud. Anot.', 'Canon FAB', 'Total']];
 
-        // Cargar la imagen y luego generar el PDF
         loadImageAsBase64(logo, (base64Image) => {
             doc.addImage(base64Image, 'PNG', 10, 5, 30, 15, '', 'FAST');
             doc.setTextColor(0, 0, 0);
             doc.text(title, titleX, 15);
-
-            // Configuración de la tabla con estilos personalizados
             doc.autoTable({
                 startY: 23,
                 head: head,
@@ -76,12 +67,9 @@ const Tarifas = () => {
                         tarifa.Principal ? `${Number(tarifa.Principal).toFixed(2)} €` : '',
                         tarifa.Auxiliar_1 ? `${Number(tarifa.Auxiliar_1).toFixed(2)} €` : '',
                     ];
-
-                    // Agregar Auxiliar 2 solo si showAuxiliar2InPDF es true
                     if (showAuxiliar2InPDF) {
                         row.push(tarifa.Auxiliar_2 ? `${Number(tarifa.Auxiliar_2).toFixed(2)} €` : '');
                     }
-
                     row.push(
                         tarifa.Anotador ? `${Number(tarifa.Anotador).toFixed(2)} €` : '',
                         tarifa.Cronometrador ? `${Number(tarifa.Cronometrador).toFixed(2)} €` : '',
@@ -90,7 +78,6 @@ const Tarifas = () => {
                         tarifa.Canon_FAB ? `${Number(tarifa.Canon_FAB).toFixed(2)} €` : '',
                         `${Number(tarifa.Total).toFixed(2)} €`
                     );
-
                     return row;
                 }),
                 styles: {
@@ -128,10 +115,12 @@ const Tarifas = () => {
     };
 
     return (
-        <div className="tarifas-container" ref={contentRef}>
-            <h1 className="title">Tarifas Arbitrajes - Temporada {season}</h1>
+        <div className="tarifas-container">
+            <h1 className="tarifas-title">Tarifas Arbitrajes - Temporada {season}</h1>
 
-            <table>
+            <button className="button" onClick={downloadPDF}><FaDownload />Descargar PDF</button>
+
+            <table className="tarifas-table">
                 <thead>
                     <tr>
                         <th>Categoría</th>
@@ -141,7 +130,7 @@ const Tarifas = () => {
                         <th>Anotador</th>
                         <th>Cronometrador</th>
                         <th>24 segundos</th>
-                        <th>Ayudante de Anotador</th>
+                        <th>Ayud. Anotador</th>
                         <th>Canon FAB</th>
                         <th>Total</th>
                     </tr>
@@ -164,7 +153,6 @@ const Tarifas = () => {
                 </tbody>
             </table>
 
-            <button onClick={downloadPDF}>Descargar PDF</button>
         </div>
     );
 };
